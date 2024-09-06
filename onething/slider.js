7cc97048808481e1675ac9c329809e5e25dcce4c
RA-L-PH <123739852+RA-L-PH@users.noreply.github.com>
2024-09-06 23:14:10 +0530

    newer

--- onething/slider.js
+++ onething/slider.js
@@ -13,3 +13,21 @@ var swiper = new Swiper(".mySwiper", {
       slideShadows: false,
     },
   });
+
+  
+
+  function imageslid(){
+    var slides = document.getElementById('imageslide');
+  
+    for(i=1; i<=10;i++){
+      slides.innerHTML += `
+        <div class="swiper-slide">
+          <a href="https://swiperjs.com/demos/images/nature-${i}.jpg">
+            <img src="https://swiperjs.com/demos/images/nature-${i}.jpg" />
+          </a>
+        </div>
+      `;
+    };
+  }
+  
+  imageslid();
